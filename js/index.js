a40548a4812d50d9d5bc3ea1fd4e2ee763810575
Biaggio <porpramuk.biaggio@gmail.com>
2021-09-03 14:15:26 -0500

    se agrego gitignore

---
+++ js/index.js
@@ -0,0 +1,61 @@
+window.sr = ScrollReveal ();
+
+    sr.reveal('.navbar', {
+        duration: 5000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.reseña__info', {
+        duration: 5000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.efectofront', {
+        duration: 7000,
+        
+    });
+
+    sr.reveal('.efectoLeft', {
+        duration: 5000,
+        origin: 'left',
+        distance: '200px'
+        
+    });
+
+    /*Staff*/
+    sr.reveal('.efectoLeft_cardBjj', {
+        duration: 10000,
+        origin: 'left',
+        distance: '200px'
+    });
+
+    sr.reveal('.efectoLeft_cardThai', {
+        duration: 8000,
+        origin: 'left',
+        distance: '200px'
+    });
+
+    sr.reveal('.efectoLeft_cardCs', {
+        duration: 6000,
+        origin: 'left',
+        distance: '200px'
+    });
+
+    sr.reveal('.efectoLeft_cardCapoeira', {
+        duration: 4000,
+        origin: 'left',
+        distance: '200px'
+    });
+
+    /*Footer*/
+    sr.reveal('.logoFooter', {
+        duration: 4000,
+        rotate: {
+            x:1,
+            y:180
+        }
+    });
+
+    
